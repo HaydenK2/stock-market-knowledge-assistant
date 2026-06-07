2b6a0fc8934f1072568db6c06ccfa83e54f7e070
Hayden <haydenk643@gmail.com>
2026-06-06 23:11:48 -0500

    fix - add retrieved_chunks to fix mongoDB insert error

--- backend/routes/rag_routes.py
+++ backend/routes/rag_routes.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
-from datetime import datetime
+from datetime import datetime, timezone
 import uuid
 from backend.database import qa_collection
 from backend.rag_system.rag import run_rag
+import traceback
 
 router = APIRouter()
 
@@ -25,9 +26,9 @@ class QAResponse(BaseModel):
     query: str
     retrieved_chunks: list
     final_answer: str
-    answer_grounded: bool
-    grounding_score: float
-    latency_ms: float
+    # answer_grounded: bool
+    # grounding_score: float
+    # latency_ms: float
         
 
 
@@ -39,27 +40,29 @@ async def ask_question(request: QuestionRequest):
         print('get final_answer')
 
         # 1. Run RAG model
-        final_answer, result = await run_rag(request.question)
+        final_answer, rag_result = await run_rag(request.question)
 
-        # print(final_answer)
-        documents = result["documents"]
+        #   obtain chunks from rag run
+        documents = rag_result["documents"]
+        retrieved_chunks = [
+            {"page_content": doc.page_content, "metadata": doc.metadata}
+            for doc in documents
+        ]
 
         # 2.  build retrieved_chunks
         qa_document = {
             "eval_id": str(uuid.uuid4()),
-            "timestamp": datetime.now(),
+            "timestamp": datetime.now(timezone.utc),
             "query": request.question,
-            "retrieved_chunks": documents,
+            "retrieved_chunks": retrieved_chunks,
             "final_answer": final_answer,
         }
 
-        print("mongo insert")
-
-
+        print("mongo insert...")
         # 3. Save to MongoDB
-        # result = await qa_collection.insert_one(qa_document)
+        result = await qa_collection.insert_one(qa_document)
+        print("mongo saved!")
 
-        print("mongo saved")
         return {
             "eval_id": qa_document["eval_id"],
             "query": qa_document["query"],
@@ -72,6 +75,7 @@ async def ask_question(request: QuestionRequest):
         }
 
     except Exception as e:
+        traceback.print_exc()  # prints full traceback to terminal
         raise HTTPException(status_code=500, detail=str(e))
 
 # Get all past Q&A history
