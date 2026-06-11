c0e83ea5b4dfedb257448ad618ffd0d8e3279d59
Hayden <haydenk643@gmail.com>
2026-06-10 23:03:53 -0500

    update to have fastapi URL and update fastapi deploy workflow

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // create instance of axios with base URL
 const api = axios.create({
-    baseURL: "http://localhost:8000"
+    baseURL:  import.meta.env.VITE_API_URL || "http://localhost:8000"
 })
 
 // Export the Axios Instance
